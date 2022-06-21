ee4fdbb36e0ae6158c6440079753298ef06e7af8
lamekammi <kameronnb@gmail.com>
2022-06-21 15:02:17 -0700

    adding a show.ejs for each flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,7 +3,8 @@ const Flight = require('../models/flight');
 module.exports = {
     new: newFlight,
     create,
-    index
+    index,
+    show
 }
 
 function newFlight(req, res) {
@@ -24,6 +25,14 @@ function index(req, res) {
     Flight.find({}, function(err, flights) {
         res.render('flights/index', {
             flights
+        })
+    })
+}
+
+function show(req, res) {
+    Flight.findById(req.params.id, function(err, flight) {
+        res.render('flights/show', {
+            flight
         })
     })
 }
